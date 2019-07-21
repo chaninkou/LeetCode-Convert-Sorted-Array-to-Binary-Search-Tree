71d59cff207de4515447b5714549324e0454212b
chaninkou <michaelkoulove@yahoo.com>
2019-07-21 15:37:51 -0700

    Found out a way to print out the tree. Need to review on this.

--- src/bst/Main.java
+++ src/bst/Main.java
@@ -1,6 +1,8 @@
 package bst;
 
 import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Main {
 	public static void main (String[] args){
@@ -12,6 +14,40 @@ public class Main {
 		
 		TreeNode tree = solution.sortedArrayToBST(nums);
 		
-		System.out.println("Solution: " + "[" +  tree.val + tree.left.val + tree.right.val + tree.left.left.val + tree.left.right.val + tree.right.left.val + tree.right.right.val + "]");
+		String solutionTree = treeNodeToString(tree);
+		
+		System.out.println("Solution: " + solutionTree);
+		
+		
 	}
+	
+	public static String treeNodeToString(TreeNode root) {
+        if (root == null) {
+            return "[]";
+        }
+    
+        String output = "";
+        
+        Queue<TreeNode> nodeQueue = new LinkedList<>();
+        
+        nodeQueue.add(root);
+        
+        while(!nodeQueue.isEmpty()) {
+            TreeNode node = nodeQueue.remove();
+    
+            if (node == null) {
+              output += "null, ";
+              continue;
+            }
+    
+            output += String.valueOf(node.val) + ", ";
+            
+            nodeQueue.add(node.left);
+            
+            nodeQueue.add(node.right);
+        }
+        
+        return "[" + output.substring(0, output.length() - 2) + "]";
+    }
+	
 }
